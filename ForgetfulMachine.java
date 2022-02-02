8ae1ebd45900e4c68f832a21d7fedffad30e7870
Desiree <desiree.vanassche@gmail.com>
2022-02-02 13:10:35 -0700

    Exercise 7 program completed.

---
+++ ForgetfulMachine.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; //allows us to recieve data from user
+
+public class ForgetfulMachine {
+  public static void main(String[] args){
+
+    Scanner keyboard = new Scanner(System.in); /* This is a scanner object and the
+    scanner is named "keyboard" the name can be changed but the same name must be used
+    throughout the program or it will create an error */
+
+    System.out.println("What city is the capital of France?");
+    keyboard.next();
+
+    System.out.println("What is 6 multiplied by 7?");
+    keyboard.nextInt();
+
+    System.out.println("Enter a numnber between 0.0 and 1.0.");
+    keyboard.nextDouble();
+
+    System.out.println("Is there anything else you would like to say?");
+    keyboard.next();
+
+  }
+}
