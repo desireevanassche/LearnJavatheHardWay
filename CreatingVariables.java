44ed414110041bcd7095ea0b150f9e59c8c1e41e
Desiree <desiree.vanassche@gmail.com>
2022-02-02 10:34:48 -0700

    Study drill for Creating Variables completed.

--- CreatingVariables.java
+++ CreatingVariables.java
@@ -1,20 +1,25 @@
 public class CreatingVariables {
   public static void main(String[] args) {
 
-  int x, y, age;
-  double seconds, e, checking;
-  String firstName, lastName, title;
+  int x, y, age, zip;
+  double seconds, e, checking, gasPrice;
+  String firstName, lastName, title, city, state;
 
   x = 10;
   y = 400;
   age = 39;
+  zip = 80122;
+
   seconds = 4.71;
   e = 2.71828182845904523536;
   checking = 1.89;
+  gasPrice = 3.79;
 
   firstName = "Desiree ";
   lastName = "VanAssche ";
   title = "Ms. ";
+  city = "Littleton";
+  state = "Colorado";
 
   System.out.println("The variable x contains " + x);
   System.out.println("The value " + y + " is stored in the varibale y.");
@@ -22,8 +27,10 @@ public class CreatingVariables {
   System.out.println("A favorite irrational # is Euler's number " + e);
   System.out.println("Hopefully you have more than $" + checking + "!");
   System.out.println("My name's " + title + firstName + lastName);
-
-  //Variable practice 
-
+  System.out.println("In the zip code " + zip + " gas is " + gasPrice + " per gallon.");
+  System.out.println(zip + " is located in " + city + ", " + state + ".");
+  
+/* added four more variables, 1 integer, 1 double, and 2 strings to complete the
+study drill */
 }
 }
