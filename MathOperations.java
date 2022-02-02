b5a983695429c2f9b43d1de67f01be5c16b2a46b
Desiree <desiree.vanassche@gmail.com>
2022-02-02 11:09:31 -0700

    Study drill completed for MathOperations.java

--- MathOperations.java
+++ MathOperations.java
@@ -1,8 +1,8 @@
 public class MathOperations {
   public static void main(String[] args) {
 
-  int a, b, c, d, e, f, g;
-  double x, y, z;
+  int a, b, c, d, e, f, g, h;
+  double x, y, z, k, l, m;
   String one, two, both;
 
   a = 10;
@@ -37,8 +37,19 @@ public class MathOperations {
   one = "dog";
   two = "house";
   both = one + two;
-  System.out.println(both);
+  System.out.println(both); //Skill drill ends here
 
-//Skill drill 
+  /* Study drill starts here: Add 2 new variables, then two new lines,
+  use mathmatical expression and use all math operators at least once */
+
+  h = 16;
+  k = 4.4;
+
+  l = (h / k) * k;
+  System.out.println("(h / k) * k is " + l);
+
+  m = (l + h) % 2;
+  System.out.println("(l + h) % 2 is " + m);
+
   }
 }
