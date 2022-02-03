1eb6fcca6b8840a358bc67c1e880c4dd878c02d3
Desiree <desiree.vanassche@gmail.com>
2022-02-03 15:03:33 -0700

    Exercise 10, found mistake and make corrections. Program is working as expected.

--- Sequencing.java
+++ Sequencing.java
@@ -5,13 +5,21 @@ public class Sequencing {
   //Broken code!
 
   Scanner keyboard = new Scanner(System.in);
-  double price =  0, salesTax, total;
+  double price, salesTax, total;
 
-  salesTax = price * 0.0825;
-  total = price + salesTax;
+/* I think the logical error was created in line 8: double price = 0, salesTax, total
+This program is intended to collect the users input for the price value */
+
 
   System.out.print("How much is the purchase price? ");
   price = keyboard.nextDouble();
+
+/* Since price is defined by the users input I moved the areas that the value
+price is used to below where it is defined and given a value. These two correction
+made the program operate as expected.*/
+
+  salesTax = price * 0.0825;
+  total = price + salesTax;
 
   System.out.println("Item price:\t" + price);
   System.out.println("Sales tac:\t" + salesTax);
