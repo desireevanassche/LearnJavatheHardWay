010c82a2ea802a80d26e509c078a2e790d560d64
Desiree <desiree.vanassche@gmail.com>
2022-02-03 14:25:41 -0700

    Exercise 9: Calculations with User Input study drill completed.

--- BMICalculator.java
+++ BMICalculator.java
@@ -4,16 +4,27 @@ public class BMICalculator {
   public static void main(String[] args) {
 
     Scanner keyboard = new Scanner(System.in);
-    double m, kg, bmi;
+    int feet;
+    double inches, lbs, bmi, height;
 
-    System.out.print("Your height in m: ");
-    m = keyboard.nextDouble();
+    System.out.print("Your height (feet only): ");
+    feet = keyboard.nextInt();
 
-    System.out.print("Your weight in kg: ");
-    kg = keyboard.nextDouble();
+    System.out.print("Your height (inches): ");
+    inches = keyboard.nextDouble();
 
-    bmi = kg / (m * m);
+    height = (feet * 12) + inches;
+    /* this formula will convert feet to inches and then add the inches remaining
+    for a total heigh in inches making a cleaner calculation for the BMI*/ 
+
+    System.out.print("Your weight in pounds: ");
+    lbs = keyboard.nextDouble();
+
+    bmi = (lbs / height / height) * 703;
+
+    // retieved BMI formula from the CDC
 
     System.out.println("Your BMI is " + bmi);
+
   }
 }
