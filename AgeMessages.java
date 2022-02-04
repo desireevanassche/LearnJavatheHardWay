b49320a593cbc83e572a59a8a0a1792148f03d7c
Desiree <desiree.vanassche@gmail.com>
2022-02-04 14:30:49 -0700

    Exercise 15: Making Decisions with If Stamenents study drill completed.

--- AgeMessages.java
+++ AgeMessages.java
@@ -13,21 +13,51 @@ public class AgeMessages {
     if (age < 13) {
     System.out.println("\ttoo young to create a Facebook account");
     }
+    //#3 For each if statment add another if statment that says the opposite
+    if (age >= 13) {
+      System.out.println("\told enough to create a Facebook");
+    }
+
     if (age < 16) {
       System.out.println("\ttoo young to get a drivers license");
+    }
+
+    if (age >= 16 ) {
+      System.out.println("\told enough to get a drivers license");
     }
 
     if (age < 18) {
       System.out.println("\ttoo young to get a tatoo");
     }
 
+    if (age >= 18) {
+      System.out.println("\told enough to get a tatoo");
+    }
+
     if (age < 21) {
       System.out.println("\ttoo young to drink alcahol");
     }
-    if ( age < 35) {
+
+    if (age >= 21) {
+      System.out.println("\told enough to drink alcahol");
+    }
+
+    if (age < 35) {
       System.out.println("\ttoo young to run for President of the U.S.");
       System.out.println("\t\t(How sad!)");
     }
 
+    if (age >= 35){
+      System.out.println("\told enough to run for President of the U.S.");
+    }
+    
+    if (age >= 65) {
+      System.out.println("\tYou are old enough to retire!");
+      //2. New condition added
+    }
+
+/*Study drill: 1. If you type in an age greater than 35 what gets printed? Why?
+You are: is printed since the println is created before the if statments start and
+there is not currently a condition that matches the input age. */
   }
 }
