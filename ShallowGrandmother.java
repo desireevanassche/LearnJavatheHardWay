14154c211de920b90ffe964ceccd87ecd4dcbf6e
Desiree <desiree.vanassche@gmail.com>
2022-02-04 07:57:24 -0700

    Exercise 14: Compound Boolean Expressions study drill complete.

--- ShallowGrandmother.java
+++ ShallowGrandmother.java
@@ -5,8 +5,8 @@ public class ShallowGrandmother {
 
     Scanner keyboard = new Scanner(System.in);
     int age;
-    double income, cute;
-    boolean allowed;
+    double income, cute, happy;
+    boolean allowed, allowed2;
 
     System.out.println("Enter your age: ");
     age = keyboard.nextInt();
@@ -17,9 +17,19 @@ public class ShallowGrandmother {
     System.out.println("How cute are you on a scale from 0.0 to 10.0? ");
     cute = keyboard.nextDouble();
 
-    allowed = (age > 25 && age < 40 && (income > 50000 || cute >= 8.5));
+    System.out.println("How happy do they make you on a scale from 0.0 to 10? ");
+    happy = keyboard.nextDouble();
 
-    System.out.println("Allowed to date my grandchild? " + allowed);
+    allowed = (age > 25 && age < 40 && (income > 50000 || cute >= 8.5));
+    System.out.println("Grandma 1: Allowed to date my grandchild? " + allowed);
+
+    /* Study drill: many people have two grandmothers, lets pretend the other
+    grandmother just wants you to be happy. Add a happiness question with a
+    scale, a new boolean variable, and write the new expression that is true if
+    persons age is close and they make you happy. */
+
+    allowed2 = (age > 25 && age <40 && (happy >= 7));
+    System.out.println("Grandma 2: Allowed to day my grandchild? " + allowed2);
 
   }
 }
