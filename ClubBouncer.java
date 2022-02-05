d05aebcc0a0197f0b2aa867a46d1ddc1e15b900c
Desiree <desiree.vanassche@gmail.com>
2022-02-05 09:23:43 -0700

    Exercise 17: Otherwise (If Statments with Else) study drill completed.

--- ClubBouncer.java
+++ ClubBouncer.java
@@ -1,6 +1,7 @@
 public class ClubBouncer {
 
   public static void main(String[] args) {
+
     int age = 22;
     boolean onGuestList = false;
     double allure = 7.5;
@@ -9,10 +10,12 @@ public class ClubBouncer {
     if (onGuestList || age >= 21 || (gender.equals("F") && allure >= 8)) {
       System.out.println("You are allowed to enter the club.");
     }
+    /* Study Drill: add a println in here and see if it compiles -
+    this generates an error code 'else without if' and does not compile. */
 
     else {
       System.out.println("You are not allowed in the club.");
-      
     }
+
   }
 }
