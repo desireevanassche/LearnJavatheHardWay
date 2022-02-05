a9ec7bca1386e018cea4fe5f16366f703533e6d0
Desiree <desiree.vanassche@gmail.com>
2022-02-05 11:57:07 -0700

    Exercise 18: If Statments with Strings study drill complete.

--- SecretWord.java
+++ SecretWord.java
@@ -4,12 +4,16 @@ public class SecretWord {
   public static void main(String[] args) {
 
     Scanner keyboard = new Scanner(System.in);
-    String secret = "please", guess;
+    String secret = "please", guess, potao;
 
-    System.out.print("What's the secret word?");
+    System.out.print("What's the secret word? ");
     guess = keyboard.next();
 
-    if (guess.equals(secret)) {
+    /*Study Drill: Pick a second keyword that will also work, the add an OR
+    to the first if statment so thay typiing either will result in an
+    "That's Correct" answer. New secret work: potato */
+
+    if (guess.equals(secret) || guess.equals("potato")) {
       System.out.println("That's correct!");
     }
 
