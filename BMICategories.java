ed242417b1e528be2e1301cd7d7a6926b1097b29
Desiree <desiree.vanassche@gmail.com>
2022-02-05 12:57:00 -0700

    Exercise 19: Mutual Exclussion with Chains of If and Else study drill completed.

--- BMICategories.java
+++ BMICategories.java
@@ -4,14 +4,31 @@ public class BMICategories {
   public static void main(String[] args){
 
     Scanner keyboard = new Scanner(System.in);
-    double bmi;
+    double bmi, inches, lbs, height;
+    int feet;
 
-    System.out.print("Enter your BMI: ");
-    bmi = keyboard.nextDouble();
+    System.out.print("Enter your height (in feet only): ");
+    feet = keyboard.nextInt();
+
+    System.out.print("Enter your height (in inches): ");
+    inches = keyboard.nextDouble();
+
+    height = (feet * 12) + inches;
+
+    System.out.print("Your weight in pounds: ");
+    lbs = keyboard.nextDouble();
+
+    bmi = (lbs / height / height) * 703;
+    System.out.println("Your BMI is: " + bmi);
 
     if (bmi < 15.0) {
+
       System.out.println("Very severly underweight");
     }
+    /* Study Drill 1. Remove the else from the if statment on line 22 and
+    see what happens. Removing the else if breaks rank and will display
+    multiple outputs.*/
+
     else if (bmi <= 16.0) {
       System.out.println("severly underweight");
     }
